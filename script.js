d28ff91568da3c656414483520c2126d321c2a08
yankawayan <124237802+yankawayan@users.noreply.github.com>
2026-03-05 22:17:03 +0900

    add check code

--- script.js
+++ script.js
@@ -31,6 +31,8 @@ startBtn.addEventListener("click", async () => {
   } 
 
   mediaRecorder.onstop = () => {
+    console.log(audioChunks);
+
     //const audioBlob = new Blob(audioChunks, { type: "audio/webm" });
     const audioBlob = new Blob(audioChunks, { type: "audio/webm;codecs=opus" });
     const audioUrl = URL.createObjectURL(audioBlob);
